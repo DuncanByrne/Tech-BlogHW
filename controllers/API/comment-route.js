206a891b4099e7274c61c1562a3aea1f10197e15
Duncan Byrne <duncanbyrne@Duncans-MacBook-Pro.local>
2021-09-23 18:54:02 -0400

    updated the js folders in controllers

--- controllers/API/comment-route.js
+++ controllers/API/comment-route.js
@@ -1,15 +1,35 @@
 const router = require("express").Router();
-const { Comment } = require("../../models/");
-const withAuth = require("../../utils/auth");
+const { Comment } = require("../../models");
 
-router.post("/", withAuth, (req, res) => {
-  Comment.create({ ...req.body, userId: req.session.userId })
-    .then(newComment => {
-      res.json(newComment);
-    })
-    .catch(err => {
-      res.status(500).json(err);
-    });
+
+router.post("/", async (req, res) => {
+	let responseBasket;
+
+	try {
+		const newComment = await Comment.create(req.body);
+		if (!newComment) {
+			responseBasket = {
+				status: res.status,
+				error: true,
+				message: "Unable to comment",
+			};
+			return res.status(200).json(responseBasket);
+		}
+
+		responseBasket = {
+			error: false,
+			message: "Comment successful",
+		};
+		return res.status(201).json(responseBasket);
+	} catch (error) {
+		responseBasket = {
+			error: true,
+			message:
+				"Try posting this comment agian.",
+		};
+
+		return res.status(408).json(responseBasket);
+	}
 });
 
 module.exports = router;
